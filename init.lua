6bd67ab251a23cb67613b0e52592436c5a87e3d9
Raphael Ahrens <raphaelahrens@googlemail.com>
2021-10-19 05:50:15 +0200

    Removed neogit because of crashes

--- init.lua
+++ init.lua
@@ -186,32 +186,6 @@ for ls, cfg in pairs({
 }) do lsp[ls].setup(cfg) end
 
 
-local neogit = require("neogit")
-
-neogit.setup {
-  signs = {
-    -- { CLOSED, OPENED }
-    
-    section = { "", "" },
-    item = { "", "" },
-    hunk = { "", "" },
-  },
-  integrations = {
-    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
-    -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    -- use { 
-    --   'TimUntersberger/neogit', 
-    --   requires = { 
-    --     'nvim-lua/plenary.nvim',
-    --     'sindrets/diffview.nvim' 
-    --   }
-    -- }
-    --
-    diffview = true  
-  },
-}
 
 -- Key mappings
 g.mapleader = 'ä'
