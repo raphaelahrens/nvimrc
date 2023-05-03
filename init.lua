002069b26f9a174377f7879384a602084b91c9d2
Raphael Ahrens <tant@tantalos.fritz.box>
2023-05-03 17:59:30 +0200

    Updated init and added descriptions

--- init.lua
+++ init.lua
@@ -2,38 +2,46 @@
 -- github.com/ojroques
 
 -------------------- HELPERS -------------------------------
-local api, cmd, fn, g = vim.api, vim.cmd, vim.fn, vim.g
-local opt, wo = vim.opt, vim.wo
+local cmd = vim.cmd
+local fn = vim.fn
+local g =  vim.g
+local opt = vim.opt
 
 local home = os.getenv('HOME')
-local nvimrc = home ..'/.config/nvim/'
+local nvimrc = home .. '/.config/nvim/'
 local pack_dir = nvimrc .. 'pack/default/start/'
 
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then
-      options = vim.tbl_extend('force', options, opts)
-  end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
+local function map(mode, lhs, rhs, desc, opts)
+    local options = {
+        noremap = true,
+        desc = desc
+    }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
 end
 
-local function nmap(lhs, rhs, opts)
-    map('n', lhs, rhs, opts)
+local function nmap(lhs, rhs, desc, opts)
+    map('n', lhs, rhs, desc, opts)
 end
-local function vmap(lhs, rhs, opts)
-    map('v', lhs, rhs, opts)
+
+local function vmap(lhs, rhs, desc, opts)
+    map('v', lhs, rhs, desc, opts)
 end
-local function imap(lhs, rhs, opts)
-    map('i', lhs, rhs, opts)
+
+local function imap(lhs, rhs, desc, opts)
+    map('i', lhs, rhs, desc, opts)
 end
-local function tmap(lhs, rhs, opts)
-    map('t', lhs, rhs, opts)
+
+local function tmap(lhs, rhs, desc, opts)
+    map('t', lhs, rhs, desc, opts)
 end
 
 g.mapleader = 'ä'
 
 local function add_word(word)
-    local aw_exit = function ()
+    local aw_exit = function()
         local filename = pack_dir .. '/spell/spell/.last-spell'
         local f = assert(io.open(filename, 'r'))
         local changedDict = f:read('*all')
@@ -45,7 +53,7 @@ local function add_word(word)
         vnew
         setl nospell
     ]])
-    fn.termopen('addword --choose "' .. word ..'"',
+    fn.termopen('addword --choose "' .. word .. '"',
         {
             on_exit = aw_exit,
             buffer_nr = fn.bufnr('%')
@@ -53,22 +61,22 @@ local function add_word(word)
     )
 end
 
-function n_add_word()
+local function n_add_word()
     local word = fn.expand('<cword>')
     add_word(word)
 end
 
-local indent= 4
+local indent = 4
 
-opt.backspace = {'indent','eol','start'}
+opt.backspace = { 'indent', 'eol', 'start' }
 opt.history = 10000
 opt.timeoutlen = 300
 opt.hidden = true
 opt.number = true
 opt.showcmd = true
 opt.wildmenu = true
-opt.wildmode = {'longest','list','full'}
-opt.completeopt = {'preview','menu','menuone','noselect'}
+opt.wildmode = { 'longest', 'list', 'full' }
+opt.completeopt = { 'preview', 'menu', 'menuone', 'noselect' }
 opt.scrolloff = 5
 opt.formatoptions = 'tcqj'
 opt.mouse = 'a'
@@ -81,6 +89,7 @@ opt.listchars = {
 opt.tabstop = indent
 opt.shiftwidth = indent
 opt.expandtab = true
+opt.updatetime = 250
 
 
 -- cmd [[colorscheme substrata]]
@@ -97,29 +106,29 @@ require('nord').set()
 require('which-key').setup({})
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
-    additional_vim_regex_highlighting = false,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+    ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = {}, -- list of language that will be disabled
+        additional_vim_regex_highlighting = false,
     },
-  },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+        },
+    },
 }
 
-require('telescope').setup{
-  pickers = {
-    man_pages = {
-      man_cmd = {"apropos", " "},
-    }
-  },
+require('telescope').setup {
+    pickers = {
+        man_pages = {
+            man_cmd = { 'apropos', ' ' },
+        }
+    },
 }
 
 require('gitsigns').setup()
@@ -127,164 +136,196 @@ require('gitsigns').setup()
 require('crates').setup()
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
 local luasnip = require('luasnip')
-local snippet = require("snippet")
+local snippet = require('snippet')
 luasnip.snippets = snippet
-local cmp = require'cmp'
+local cmp = require 'cmp'
 cmp.setup({
-  snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      end,
-  },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    preselect = cmp.PreselectMode.None,
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = function(fallback)
+            if cmp.visible() then
+                cmp.confirm({ select = true })
+            else
+                cmp.complete()
+            end
+        end,
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-
-  },
---  sources = cmp.config.sources({
---     { name = 'nvim_lsp' },
---    },
---    {
---     { name = 'path' },
---     { name = 'spell' },
---     { name = 'buffer' },
---  }),
-  sources = cmp.config.sources(
-      {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = "crates" },
-      },{
-          { name = 'buffer' },
-      },{
-          { name = 'path' },
-          { name = 'spell' },
-      }
-  )
+    sources = cmp.config.sources(
+        {
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'crates' },
+        }, {
+            { name = 'buffer' },
+        }, {
+        { name = 'path' },
+        { name = 'spell' },
+    }
+    )
 })
 
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.code_actions.shellcheck
+    },
+})
 
 -- opt.foldmethod='expr'
 -- opt.foldexpr='nvim_treesitter#foldexpr()'
 
 local on_attach = function(client)
-    require "lsp_signature".on_attach()  -- Note: add in lsp client on-attach
+    vim.api.nvim_create_autocmd('CursorHold', {
+        buffer = bufnr,
+        callback = function()
+            local opts = {
+                focusable = false,
+                close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+                border = 'rounded',
+                source = 'always',
+                prefix = ' ',
+                scope = 'cursor',
+            }
+            vim.diagnostic.open_float(nil, opts)
+        end
+    })
+    require 'lsp_signature'.on_attach() -- Note: add in lsp client on-attach
 end
+
+vim.lsp.set_log_level("debug")
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lsp = require('lspconfig')
 for ls, cfg in pairs({
-  ccls = {
-      on_attach = on_attach,
+    rust_analyzer = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
     },
-  jsonls = {
-      on_attach = on_attach,
+    pylsp = {
+        on_attach = on_attach,
+        capabilities = capabilities,
     },
-  rust_analyzer = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        assist = {
-            importGranularity = "module",
-            importPrefix = "by_self",
-        },
-        cargo = {
-            loadOutDirsFromCheck = true
-        },
-        procMacro = {
-            enable = true
-        },
-      }
-  },
-  pylsp = {
-      on_attach = on_attach,
-      capabilities = capabilities,
+    sumneko_lua = {
+        on_attach = on_attach,
+        capabilities = capabilities,
     },
 }) do lsp[ls].setup(cfg) end
 
 
 
 -- Key mappings
-nmap('Q', 'gq')
-nmap('Y', 'y$')
+nmap('Q', 'gq', 'Format lines `gq`')
+-- nmap('Y', 'y$', '')
 
-nmap('ü', '"0p')
-nmap('Ü', '"0P')
-vmap('ü', '"0p')
-vmap('Ü', '"0P')
+nmap('ü', '"0p', 'paste last yank')
+nmap('Ü', '"0P', 'paste last yank')
+vmap('ü', '"0p', 'paste last yank')
+vmap('Ü', '"0P', 'paste last yank')
 
-nmap('°', '^')
+nmap('°', '^', 'Go to start of the line')
 -- nmap('<leader>l', '<cmd>!leo <C-r><C-w><CR>')
 
-nmap('<leader>ö', '', {callback=n_add_word})
+nmap('<leader>ö', n_add_word, 'Add word to dictionary')
 
 
-nmap('<F7>', '<cmd>TagbarToggle<CR>')
-nmap('<F12>', '<cmd>split ~/notes<CR>')
+nmap('<F7>', '<cmd>TagbarToggle<CR>', '')
+-- nmap('<F12>', '<cmd>split ~/notes<CR>')
 
-imap('<A-Down>', '<Esc>gja')
-imap('<A-Up>', '<Esc>gka')
-vmap('<A-Down>', 'gj')
-vmap('<A-up>', 'gk')
-nmap('<A-Down>', 'gj')
-nmap('<A-Up>', 'gk')
+imap('<A-Down>', '<Esc>gja', 'move cursor down in wrap lines')
+imap('<A-Up>', '<Esc>gka', 'move cursor up in wrap lines')
+vmap('<A-Down>', 'gj', 'move cursor down in wrap lines')
+vmap('<A-Up>', 'gk', 'move cursor up in wrap lines')
+nmap('<A-Down>', 'gj', 'move cursor down in wrap lines')
+nmap('<A-Up>', 'gk', 'move cursor up in wrap lines')
 
-nmap('<C-Left>', ':previous<CR>')
-nmap('<C-Right>', ':next<CR>')
-nmap('<C-S-Left>', ':cprevious<CR>')
-nmap('<C-S-Right>', ':cnext<CR>')
+nmap('<C-Left>', ':previous<CR>', 'Goto previous buffer in arglist')
+nmap('<C-Right>', ':next<CR>', 'Goto tp the next biffer in the arglist')
+nmap('<C-S-Left>', ':cprevious<CR>', ' Got to previous buffer in the bufferlist')
+nmap('<C-S-Right>', ':cnext<CR>', ' Got to next buffer in the bufferlist')
 
-nmap('<Tab>', '>>_')
-nmap('<S-Tab>', '<<_')
-imap('<S-Tab>', '<C-D>')
-vmap('<Tab>', '>gv')
-vmap('<S-Tab>', '<gv')
+nmap('<Tab>', '>>_', 'ident line')
+nmap('<S-Tab>', '<<_', 'indent line')
+-- imap('<S-Tab>', '<C-D>')
+vmap('<Tab>', '>gv', 'indent visual selection')
+vmap('<S-Tab>', '<gv', 'indent visual selection')
 
 -- Move visual block
-vmap('J', ":m '>+1<CR>gv=gv")
-vmap('K', ":m '<-2<CR>gv=gv")
+vmap('J', ":m '>+1<CR>gv=gv", 'move visual selection down')
+vmap('K', ":m '<-2<CR>gv=gv", 'move visual selection up')
 
 -- Use <C-L> to clear the highlighting of :set hlsearch.
-nmap('<C-L>', ':nohlsearch<CR><C-L>')
-nmap('<Leader>*', ':Grepper -cword -noprompt<CR>')
+nmap('<C-L>', ':nohlsearch<CR><C-L>', 'clear higlight')
 
 tmap('<ESC>', '<C-\\><C-n>')
 tmap('<C-v><ESC>', '<ESC>')
 
+require('which-key').register({
+    f = {
+        name = 'find with Telescope'
+    },
+    l = {
+        name = 'LSP'
+    },
+}, { prefix = '<leader>' }
+)
 
-nmap('<leader>l,', '', {callback=vim.lsp.diagnostic.goto_prev})
-nmap('<leader>l.', '', {callback=vim.lsp.diagnostic.goto_next})
-nmap('<leader>la', '', {callback=vim.lsp.buf.code_action})
-nmap('<leader>ld', '', {callback=vim.lsp.buf.definition})
-nmap('<leader>lf', '', {callback=vim.lsp.buf.formatting})
-nmap('<leader>lh', '', {callback=vim.lsp.buf.hover})
-nmap('<leader>lm', '', {callback=vim.lsp.buf.rename})
-nmap('<leader>lr', '', {callback=vim.lsp.buf.references})
-nmap('<leader>ls', '', {callback=vim.lsp.buf.document_symbol})
+nmap('<leader>la', vim.lsp.buf.code_action, 'LSP code action')
+nmap('<leader>ld', vim.lsp.buf.definition, 'LSP definition')
+nmap('<leader>lf', vim.lsp.buf.format, 'LSP formatting')
+nmap('<leader>lh', vim.lsp.buf.hover, 'LSP hover')
+nmap('<leader>lm', vim.lsp.buf.rename, 'LSP rename')
+nmap('<leader>lr', vim.lsp.buf.references, 'LSP references')
+nmap('<leader>ls', vim.lsp.buf.document_symbol, 'LSP document symbol')
 
 
-nmap('<leader>ff', '', {callback=require('telescope.builtin').find_files})
-nmap('<leader>fg', '', {callback=require('telescope.builtin').live_grep})
-nmap('<leader>fb', '', {callback=require('telescope.builtin').buffers})
-nmap('<leader>fh', '', {callback=require('telescope.builtin').help_tags})
+nmap('<leader>ff', require('telescope.builtin').find_files, 'Telescope find files')
+nmap('<leader>fg', require('telescope.builtin').live_grep, 'Telescope live grep')
+nmap('<leader>fb', require('telescope.builtin').buffers, 'Telescope buffers')
+nmap('<leader>fh', require('telescope.builtin').help_tags, 'Telescope help tags')
 
-imap('<C-J>',   '', {callback=require('luasnip').expand_or_jump})
-imap('<C-S-J>', '', {callback=function() require('luasnip').jump(-1) end})
-imap('<C-B>',   '', {callback=function() require('luasnip').change_choice(1)end})
-imap('<C-S-B>', '', {callback=function() require('luasnip').change_choice(-1)end})
+imap('<C-J>', require('luasnip').expand_or_jump, 'LuaSnip expand or jump')
+imap('<C-S-J>', function() require('luasnip').jump(-1) end, 'LuaSnip jump back')
+imap('<C-B>', function() require('luasnip').change_choice(1) end, 'LuaSnip next choice')
+imap('<C-S-B>', function() require('luasnip').change_choice(-1) end, 'LuaSnip previous choice')
 
 g.togglecursor_force = 'xterm'
